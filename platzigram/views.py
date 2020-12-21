55bb454a164831b942a26823dcdc5377b38ef872
osdn <oadrian21@gmail.com>
2020-12-21 12:34:21 -0600

    first commit

---
+++ platzigram/views.py
@@ -0,0 +1,20 @@
+""" views """
+from django.http import HttpResponse
+from datetime import datetime
+from django.http import JsonResponse
+
+def hello_world(request):
+    return HttpResponse('The time is: {now}'.format(
+        now=datetime.now().strftime('%b %d, %Y, %H:%M')
+        ))
+def hi (request):
+    nums = request.GET.get('numbers')
+    if nums == None:
+        return HttpResponse("no numbers variable")
+    nums = sorted(nums.split(','))
+    data = {
+        'status': 'ok',
+        'data': nums
+    }
+    print(nums)
+    return JsonResponse(data,safe = False)
